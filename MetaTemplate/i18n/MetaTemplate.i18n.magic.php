a79e8f86d9eeebd3a552dc7235c328220d10c2c9
Robin Hood <robinhood70@live.ca>
2022-11-25 01:19:54 -0500

    Variables (and ~) are caps and case sensitive

--- MetaTemplate/i18n/MetaTemplate.i18n.magic.php
+++ MetaTemplate/i18n/MetaTemplate.i18n.magic.php
@@ -9,19 +9,19 @@ $magicWords['en'] = [
 	MetaTemplate::NA_SHIFT => [0, 'shift', 'shiftdown'],
 
 	MetaTemplate::PF_DEFINE => [0, 'define'],
-	MetaTemplate::PF_FULLPAGENAMEx => [0, 'FULLPAGENAMEx'],
+	MetaTemplate::PF_FULLPAGENAMEx => [1, 'FULLPAGENAMEx'],
 	MetaTemplate::PF_INHERIT => [0, 'inherit'],
 	MetaTemplate::PF_LOCAL => [0, 'local'],
-	MetaTemplate::PF_NAMESPACEx => [0, 'NAMESPACEx'],
-	MetaTemplate::PF_PAGENAMEx => [0, 'PAGENAMEx'],
+	MetaTemplate::PF_NAMESPACEx => [1, 'NAMESPACEx'],
+	MetaTemplate::PF_PAGENAMEx => [1, 'PAGENAMEx'],
 	MetaTemplate::PF_PREVIEW => [0, 'preview'],
 	MetaTemplate::PF_RETURN => [0, 'return'],
 	MetaTemplate::PF_UNSET => [0, 'unset'],
 
-	MetaTemplate::VR_FULLPAGENAME0 => [0, 'FULLPAGENAME0'],
-	MetaTemplate::VR_NAMESPACE0 => [0, 'NAMESPACE0'],
-	MetaTemplate::VR_NESTLEVEL => [0, 'NESTLEVEL'],
-	MetaTemplate::VR_PAGENAME0 => [0, 'PAGENAME0'],
+	MetaTemplate::VR_FULLPAGENAME0 => [1, 'FULLPAGENAME0'],
+	MetaTemplate::VR_NAMESPACE0 => [1, 'NAMESPACE0'],
+	MetaTemplate::VR_NESTLEVEL => [1, 'NESTLEVEL'],
+	MetaTemplate::VR_PAGENAME0 => [1, 'PAGENAME0'],
 ];
 
 if (MetaTemplate::can(MetaTemplate::STTNG_ENABLECPT)) {
@@ -49,6 +49,7 @@ if (MetaTemplate::can(MetaTemplate::STTNG_ENABLEDATA)) {
 	$magicWords['en'] = array_merge($magicWords['en'], [
 		MetaTemplateData::NA_NAMESPACE => [0, 'namespace'],
 		MetaTemplateData::NA_ORDER => [0, 'order'],
+		MetaTemplateData::NA_PAGELENGTH => [0, 'pagelength'],
 		MetaTemplateData::NA_PAGENAME => [0, 'pagename'],
 		MetaTemplateData::NA_SAVEMARKUP => [0, 'savemarkup'],
 		MetaTemplateData::NA_SET => [0, 'set', 'subset'],

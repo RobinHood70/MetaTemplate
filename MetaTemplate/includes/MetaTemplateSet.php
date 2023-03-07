035c7bd688b351fa7c36555d00982fb8cd94eb58
Robin Hood <robinhood70@live.ca>
2023-03-07 11:54:30 -0500

    Fix internal type

--- MetaTemplate/includes/MetaTemplateSet.php
+++ MetaTemplate/includes/MetaTemplateSet.php
@@ -5,14 +5,14 @@ class MetaTemplateSet
 	/** @var ?string The name of the set. */
 	public $name;
 
-	/** @var ?string[] $variables */
+	/** @var string[] $variables */
 	public $variables = [];
 
 	/**
 	 * Creates an instance of the MetaTemplateSet class.
 	 *
 	 * @param ?string $name The name of the set to create.
-	 * @param ?string[] $variables Any variables to pre-initialize the set with.
+	 * @param string[] $variables Any variables to pre-initialize the set with.
 	 */
 	public function __construct(?string $name = null, array $variables = [])
 	{
